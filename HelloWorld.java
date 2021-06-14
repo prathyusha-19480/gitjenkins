0e40119e27fa87fcf27e69d814f223aecbb3ddf0
prathyusha chadalapaka <prathyu.0998@gmail.com>
2021-06-14 08:38:33 +0530

    modeified

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld{
 	public static void main(String a[]){
 		System.out.println("Hello World");
                 System.out.println("Hello DevOps");
+              System.out.println("welcome to DevOps");
 
 	}
 }
